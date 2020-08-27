93d53e05d0fd0e53c864e1d87fdda16a9037c87d
soeur <juzi201314@gmail.com>
2020-08-27 12:47:02 +0800

    upload pkg

---
+++ pkg/deno_bincode.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {Uint8Array} data
+* @returns {any}
+*/
+export function decode(data: Uint8Array): any;
+/**
+* @param {any} v
+* @returns {Uint8Array}
+*/
+export function encode(v: any): Uint8Array;
